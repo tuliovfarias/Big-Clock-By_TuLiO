3eb0ade45a151fdabba6f127511516ee883d5f27
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2020-10-01 19:49:10 -0300

    Criei

---
+++ Projeto BigClock/Programa/frist_web_sever/Automacao/index.h
@@ -0,0 +1,18 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+    html {font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}
+    .button {background-color: blue; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 60px; margin: 0px; cursor: pointer;}
+    .button2 {background-color: #77878A;}
+  </style>
+</head>
+<hr>
+<h1>CRONOMETRAR</h1><br>
+<p><a href="/cron_play"><button class="button">INICIAR</button></a></p><br>
+<p><a href="/cron_reset"><button class="button">ZERAR</button button2></a></p><br>
+<hr>
+</html>
+)=====";
