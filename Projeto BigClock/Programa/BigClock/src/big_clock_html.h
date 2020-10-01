2260a8fb10c593f48e7fb4fd090d7dd28aecd5f4
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2020-10-01 20:13:11 -0300

    organizando

---
+++ Projeto BigClock/Programa/BigClock/src/big_clock_html.h
@@ -0,0 +1,40 @@
+static const char RELOGIO_page[] PROGMEM = R"=====(
+<!DOCTYPE html><html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:,">
+    <style>
+      html {font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}
+      .button {background-color: blue; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 40px; margin: 0px; cursor: pointer;}
+      .button2 {background-color: #77878A;}
+    </style>
+  </head>
+  <hr>
+  <h1>BIG CLOCK</h1><br>
+  <p><a href="/"><button class="button">HORARIO</button></a></p><br>
+  <p><a href="/"><button class="button">DATA</button></a></p><br>
+  <p><a href="/cron"><button class="button">CRONOMETRO</button></a></p><br>
+  <p><a href="/"><button class="button">ALARME</button button2></a></p><br>
+  <hr>
+</html>
+)=====";
+
+static const char CRONOMETRO_page[] PROGMEM = R"=====(
+<!DOCTYPE html><html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:,">
+    <style>
+      html {font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}
+      .button {background-color: blue; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 40px; margin: 0px; cursor: pointer;}
+      .button2 {background-color: #77878A;}
+    </style>
+  </head>
+  <hr>
+  <h1>CRONOMETRAR</h1><br>
+  <p><a href="/cron/play"><button class="button">INICIAR</button></a></p><br>
+  <p><a href="/cron/reset"><button class="button">ZERAR</button></a></p><br>
+  <p><a href="/"><button class="button">VOLTAR</button></a></p><br>
+  <hr>
+</html>
+)=====";
